acd3e685dce05b6192ca31ee6ebba7f7b51f9f93
Evelyn <53372490+CodyPenny@users.noreply.github.com>
2022-02-26 13:40:27 -0800

    Create router.js

---
+++ Product/server/router.js
@@ -0,0 +1,14 @@
+const Router = require('express').Router();
+const controller = require('./controller');
+
+
+Router.route('/')
+  .get(controller.getAll)
+
+Router.route('/sku/:sku')
+  .get(controller.getOne)
+
+Router.route('/name/:name')
+  .get(controller.getName)
+
+module.exports = Router;
